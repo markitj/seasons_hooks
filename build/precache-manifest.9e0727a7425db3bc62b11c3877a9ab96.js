c580ff8f6f8846e96a894af20dbec72f9c2107c5
markitj <msek986@gmail.com>
2019-11-08 10:24:27 +0100

    Initial commit

---
+++ build/precache-manifest.9e0727a7425db3bc62b11c3877a9ab96.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "423c2c0932020bf4e969",
+    "url": "/static/css/main.0ff597d7.chunk.css"
+  },
+  {
+    "revision": "423c2c0932020bf4e969",
+    "url": "/static/js/main.423c2c09.chunk.js"
+  },
+  {
+    "revision": "78fa1d7f359a8e9073d8",
+    "url": "/static/js/1.78fa1d7f.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "873073b60b6fed06e3a195a806e57ea2",
+    "url": "/index.html"
+  }
+];
